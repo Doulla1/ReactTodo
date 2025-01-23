f438d3faaf365a7713eccae1bb365f79284c161a
Abdoulaye <dialloabdoul99c@gmail.com>
2025-01-23 23:41:11 +0100

    fix: change checked text in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders TODO TEST title', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/TODO TEST/i);
+    expect(linkElement).toBeInTheDocument();
 });
