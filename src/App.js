b6da316946f4fa01897723b2cd3b8c6efa566ebc
Abdoulaye <dialloabdoul99c@gmail.com>
2025-01-25 15:50:06 +0100

    refactor: convert classes to functions for App.js and index and add comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { Component } from 'react';
+import React from 'react';
 import MainPage from './components/MainPage';
 import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>TODO TEST</h1>
-                <MainPage />
-            </div>
-        );
-    }
-}
+const App = () => {
+    return (
+        <div className="App">
+            <h1>TODO TEST</h1>
+            <MainPage />
+        </div>
+    );
+};
 
 export default App;
