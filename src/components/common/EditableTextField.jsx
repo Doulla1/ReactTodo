a4f46e826ebb2fb0f96d84daef88142f9b5df5f9
Abdoulaye <dialloabdoul99c@gmail.com>
2025-01-25 14:57:03 +0100

    refactor: convert EditableTextField from class to functional component with hooks and add props checking

--- src/components/common/EditableTextField.jsx
+++ src/components/common/EditableTextField.jsx
@@ -1,89 +1,89 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-class EditableTextField extends Component {
-    constructor(props) {
-        super(props);
+/**
+ * Composant EditableTextField permet de modifier et de supprimer du texte.
+ *
+ * @param {string} value - La valeur du texte à afficher et à modifier.
+ * @param {function} onChange - Fonction de rappel pour gérer la modification du texte.
+ * @param {function} onRemove - Fonction de rappel pour gérer la suppression du texte.
+ * @param {boolean} isEditButtonEnable - Indicateur pour activer/désactiver le bouton d'édition.
+ * @param {number} index - Index de l'élément (pour identification).
+ */
+const EditableTextField = ({
+    value,
+    onChange,
+    onRemove,
+    isEditButtonEnable,
+    index,
+}) => {
+    const [isEditing, setIsEditing] = useState(false);
 
-        this.state = {
-            isEditing: false,
-            value: props.value,
-        };
+    const handleBlur = () => {
+        if (value) setIsEditing(false);
+    };
 
-        this.handleBlur = this.handleBlur.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleEdit = this.handleEdit.bind(this);
-        this.handleRemove = this.handleRemove.bind(this);
-        this.handleEnterKeyDown = this.handleEnterKeyDown.bind(this);
-    }
-
-    handleBlur() {
-        const { value } = this.props;
-        if (value) this.setState({ isEditing: false });
-    }
-
-    handleChange(event) {
-        const { onChange } = this.props;
-
+    const handleChange = (event) => {
         if (onChange) {
             onChange(event);
         } else {
             console.error('onChange is not defined');
         }
-    }
+    };
 
-    handleEnterKeyDown(event) {
+    const handleEnterKeyDown = (event) => {
         if (event.key === 'Enter') {
-            this.handleBlur();
+            handleBlur();
         }
-    }
+    };
 
-    handleEdit() {
-        this.setState({ isEditing: true });
-    }
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
 
-    handleRemove(event) {
-        const { onRemove } = this.props;
+    const handleRemove = (event) => {
         if (onRemove) {
             onRemove(event);
         } else {
             console.error('onRemove is not defined');
         }
-    }
+    };
 
-    render() {
-        const { isEditing } = this.state;
-        const { value, isEditButtonEnable, index } = this.props;
+    return isEditing ? (
+        <input
+            type="text"
+            value={value}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onKeyDown={handleEnterKeyDown}
+            data-index={index}
+        />
+    ) : (
+        <>
+            <span className="editable-text-field" onClick={handleEdit}>
+                {value}
+            </span>
+            {isEditButtonEnable && <Button label="Edit" onClick={handleEdit} />}
+            <Button label="Remove" onClick={handleRemove} data-index={index} />
+        </>
+    );
+};
 
-        if (isEditing) {
-            return (
-                <input
-                    type="text"
-                    value={value}
-                    onBlur={this.handleBlur}
-                    onChange={this.handleChange}
-                    onKeyDown={this.handleEnterKeyDown}
-                    data-index={index}
-                />
-            );
-        }
+EditableTextField.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    onRemove: PropTypes.func,
+    isEditButtonEnable: PropTypes.bool,
+    index: PropTypes.number,
+};
 
-        return (
-            <>
-                <span className="editable-text-field" onClick={this.handleEdit}>
-                    {value}
-                </span>
-                {isEditButtonEnable && (
-                    <Button label="Edit" onClick={this.handleEdit} />
-                )}
-                <Button
-                    label="Remove"
-                    onClick={this.handleRemove}
-                    data-index={index}
-                />
-            </>
-        );
-    }
-}
+EditableTextField.defaultProps = {
+    value: '',
+    onChange: null,
+    onRemove: null,
+    isEditButtonEnable: true,
+    index: 0,
+};
 
 export default EditableTextField;
