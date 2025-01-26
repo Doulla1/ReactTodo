c8e079478ec62251d048dc2e22165bb25572fc9a
Abdoulaye <dialloabdoul99c@gmail.com>
2025-01-26 19:46:59 +0100

    test: Add test for MainPage

--- src/__tests__/MainPage.test.js
+++ src/__tests__/MainPage.test.js
@@ -2,28 +2,28 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import MainPage from '../components/MainPage';
 
 test('renders MainPage with initial list', () => {
-  render(<MainPage />);
-  const initialList = screen.getByText(/Today/i);
-  expect(initialList).toBeInTheDocument();
+    render(<MainPage />);
+    const initialList = screen.getByText(/Today/i);
+    expect(initialList).toBeInTheDocument();
 
-  const addButton = screen.getByText(/Add list/i);
-  expect(addButton).toBeInTheDocument();
+    const addButton = screen.getByText(/Add list/i);
+    expect(addButton).toBeInTheDocument();
 });
 
 test('adds a new list when "Add list" is clicked', () => {
-  render(<MainPage />);
-  const addButton = screen.getByText(/Add list/i);
-  fireEvent.click(addButton);
+    render(<MainPage />);
+    const addButton = screen.getByText(/Add list/i);
+    fireEvent.click(addButton);
 
-  const newList = screen.getByText(/List 2/i);
-  expect(newList).toBeInTheDocument();
+    const newList = screen.getByText(/List 2/i);
+    expect(newList).toBeInTheDocument();
 });
 
 test('removes a list when "Remove" is clicked', () => {
-  render(<MainPage />);
-  const removeButton = screen.getAllByText(/Remove/i)[0];
-  fireEvent.click(removeButton);
+    render(<MainPage />);
+    const removeButton = screen.getAllByText(/Remove/i)[0];
+    fireEvent.click(removeButton);
 
-  const removedList = screen.queryByText(/Today/i);
-  expect(removedList).not.toBeInTheDocument();
+    const removedList = screen.queryByText(/Today/i);
+    expect(removedList).not.toBeInTheDocument();
 });
