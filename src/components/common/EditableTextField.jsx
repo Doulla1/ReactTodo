0f9c0171bd586b6a3fe6a83be394bbc3ea330638
Abdoulaye <dialloabdoul99c@gmail.com>
2025-01-26 17:59:02 +0100

    refactor: convert EditableTextField and TodoList from class to functionnal component

--- src/components/common/EditableTextField.jsx
+++ src/components/common/EditableTextField.jsx
@@ -1,85 +1,90 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const EditableTextField = ({ value: initialValue, onChange, onRemove, index }) => {
-	const [isEditing, setIsEditing] = useState(false);
-	const [value, setValue] = useState(initialValue);
+const EditableTextField = ({
+    value: initialValue,
+    onChange,
+    onRemove,
+    index,
+}) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [value, setValue] = useState(initialValue);
 
-	const handleBlur = () => {
-		if (value) setIsEditing(false);
-	};
+    const handleBlur = () => {
+        if (value) setIsEditing(false);
+    };
 
-	const handleChange = (event) => {
-		const newValue = event.target.value;
-		setValue(newValue);
-		if (onChange) {
-			onChange(event);
-		} else {
-			console.error('onChange is not defined');
-		}
-	};
+    const handleChange = (event) => {
+        const newValue = event.target.value;
+        setValue(newValue);
+        if (onChange) {
+            onChange(event);
+        } else {
+            console.error('onChange is not defined');
+        }
+    };
 
-	const handleEnterKeyDown = (event) => {
-		if (event.key === 'Enter') {
-			handleBlur();
-		}
-	};
+    const handleEnterKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleBlur();
+        }
+    };
 
-	const handleEdit = () => {
-		setIsEditing(true);
-	};
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
 
-	const handleRemove = (event) => {
-		if (onRemove) {
-			onRemove(event);
-		} else {
-			console.error('onRemove is not defined');
-		}
-	};
+    const handleRemove = (event) => {
+        if (onRemove) {
+            onRemove(event);
+        } else {
+            console.error('onRemove is not defined');
+        }
+    };
 
-	if (isEditing) {
-		return (
-			<input
-				type="text"
-				className="form-control w-100"
-				value={value}
-				onBlur={handleBlur}
-				onChange={handleChange}
-				onKeyDown={handleEnterKeyDown}
-				data-index={index}
-			/>
-		);
-	}
+    if (isEditing) {
+        return (
+            <input
+                type="text"
+                className="form-control w-100"
+                value={value}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                onKeyDown={handleEnterKeyDown}
+                data-index={index}
+            />
+        );
+    }
 
-	return (
-		<div className="d-flex align-items-center w-100 mb-4">
-			<span
-				className="editable-text-field me-2 flex-grow-1"
-				onClick={handleEdit}
-			>
-				{value}
-			</span>
-			<button
-				className="btn btn-danger ms-2"
-				onClick={handleRemove}
-				data-index={index}
-				style={{ cursor: 'pointer' }}
-			>
-				Supprimer
-			</button>
-		</div>
-	);
+    return (
+        <div className="d-flex align-items-center w-100 mb-4">
+            <span
+                className="editable-text-field me-2 flex-grow-1"
+                onClick={handleEdit}
+            >
+                {value}
+            </span>
+            <button
+                className="btn btn-danger ms-2"
+                onClick={handleRemove}
+                data-index={index}
+                style={{ cursor: 'pointer' }}
+            >
+                Supprimer
+            </button>
+        </div>
+    );
 };
 
 EditableTextField.propTypes = {
-	value: PropTypes.string.isRequired,
-	onChange: PropTypes.func.isRequired,
-	onRemove: PropTypes.func.isRequired,
-	index: PropTypes.number,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onRemove: PropTypes.func.isRequired,
+    index: PropTypes.number,
 };
 
 EditableTextField.defaultProps = {
-	index: 0,
+    index: 0,
 };
 
 export default EditableTextField;
--- src/components/common/TodoList.jsx
+++ src/components/common/TodoList.jsx
@@ -4,114 +4,114 @@ import EditableTextField from './EditableTextField';
 import Button from './Button';
 
 const TodoList = () => {
-	const [todos, setTodos] = useState([]);
-	const [newTodo, setNewTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
 
-	const handleCheck = (event) => {
-		const index = event?.target?.dataset?.index ?? null;
+    const handleCheck = (event) => {
+        const index = event?.target?.dataset?.index ?? null;
 
-		if (index !== null) {
-			const newTodos = [...todos];
-			newTodos[index].checked = !newTodos[index].checked;
-			setTodos(newTodos);
-		}
-	};
+        if (index !== null) {
+            const newTodos = [...todos];
+            newTodos[index].checked = !newTodos[index].checked;
+            setTodos(newTodos);
+        }
+    };
 
-	const handleChange = (event) => {
-		setNewTodo(event?.target?.value ?? newTodo);
-	};
+    const handleChange = (event) => {
+        setNewTodo(event?.target?.value ?? newTodo);
+    };
 
-	const handleChangeTodoName = (event) => {
-		const { value, dataset } = event.target;
-		const index = dataset?.index ?? null;
+    const handleChangeTodoName = (event) => {
+        const { value, dataset } = event.target;
+        const index = dataset?.index ?? null;
 
-		if (index !== null) {
-			const newTodos = [...todos];
-			newTodos[index].value = value;
-			setTodos(newTodos);
-		}
-	};
+        if (index !== null) {
+            const newTodos = [...todos];
+            newTodos[index].value = value;
+            setTodos(newTodos);
+        }
+    };
 
-	const handleRemoveTodo = (event) => {
-		const index = event?.target?.dataset?.index ?? null;
+    const handleRemoveTodo = (event) => {
+        const index = event?.target?.dataset?.index ?? null;
 
-		if (index !== null) {
-			const newTodos = [...todos];
-			newTodos.splice(index, 1);
-			setTodos(newTodos);
-		}
-	};
+        if (index !== null) {
+            const newTodos = [...todos];
+            newTodos.splice(index, 1);
+            setTodos(newTodos);
+        }
+    };
 
-	const handleAddNewTodo = (event) => {
-		if (event) event.preventDefault();
-		const newTodoItem = {
-			checked: false,
-			value: newTodo,
-		};
+    const handleAddNewTodo = (event) => {
+        if (event) event.preventDefault();
+        const newTodoItem = {
+            checked: false,
+            value: newTodo,
+        };
 
-		if (newTodo) {
-			setTodos([...todos, newTodoItem]);
-			setNewTodo('');
-		}
-	};
+        if (newTodo) {
+            setTodos([...todos, newTodoItem]);
+            setNewTodo('');
+        }
+    };
 
-	const handleEnterKeyDown = (event) => {
-		if (event.key === 'Enter') {
-			handleAddNewTodo();
-		}
-	};
+    const handleEnterKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleAddNewTodo();
+        }
+    };
 
-	const renderATodoLine = (todo, index) => {
-		const { checked, value } = todo;
-		return (
-			<li
-				key={`todo-line-${index}`}
-				className="list-group-item d-flex align-items-center"
-			>
-				<Checkbox
-					checked={checked}
-					onChange={handleCheck}
-					data-index={index}
-				/>
-				<EditableTextField
-					value={value}
-					onChange={handleChangeTodoName}
-					onRemove={handleRemoveTodo}
-					index={index}
-					isEditButtonEnable={true}
-				/>
-			</li>
-		);
-	};
+    const renderATodoLine = (todo, index) => {
+        const { checked, value } = todo;
+        return (
+            <li
+                key={`todo-line-${index}`}
+                className="list-group-item d-flex align-items-center"
+            >
+                <Checkbox
+                    checked={checked}
+                    onChange={handleCheck}
+                    data-index={index}
+                />
+                <EditableTextField
+                    value={value}
+                    onChange={handleChangeTodoName}
+                    onRemove={handleRemoveTodo}
+                    index={index}
+                    isEditButtonEnable={true}
+                />
+            </li>
+        );
+    };
 
-	const renderNewAddingLine = () => {
-		return (
-			<li
-				key={'todo-line-add'}
-				className="list-group-item d-flex align-items-center"
-			>
-				<input
-					type="text"
-					value={newTodo}
-					onChange={handleChange}
-					onKeyDown={handleEnterKeyDown}
-					className="form-control me-2"
-				/>
-				<Button
-					label="Add todo"
-					onClick={handleAddNewTodo}
-					className="btn btn-primary"
-				/>
-			</li>
-		);
-	};
+    const renderNewAddingLine = () => {
+        return (
+            <li
+                key={'todo-line-add'}
+                className="list-group-item d-flex align-items-center"
+            >
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={handleChange}
+                    onKeyDown={handleEnterKeyDown}
+                    className="form-control me-2"
+                />
+                <Button
+                    label="Add todo"
+                    onClick={handleAddNewTodo}
+                    className="btn btn-primary"
+                />
+            </li>
+        );
+    };
 
-	return (
-		<ul className="list-group">
-			{todos.map((todo, index) => renderATodoLine(todo, index))}
-			{renderNewAddingLine()}
-		</ul>
-	);
+    return (
+        <ul className="list-group">
+            {todos.map((todo, index) => renderATodoLine(todo, index))}
+            {renderNewAddingLine()}
+        </ul>
+    );
 };
 
 export default TodoList;
